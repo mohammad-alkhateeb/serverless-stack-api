c5a6283b5c6cb6be7e66ac8d4800058daab93753
mohammad <you@example.com>
2020-08-05 13:25:40 +0900

    Adding our Serverless API

---
+++ tests/billing.test.js
@@ -0,0 +1,36 @@
+//import { calculateCost } from "../libs/billing-lib";
+
+test("Lowest tier", () => {
+  const storage = 10;
+
+  const cost = 4000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Middle tier", () => {
+  const storage = 100;
+
+  const cost = 20000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Highest tier", () => {
+  const storage = 101;
+
+  const cost = 10100;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+function calculateCost(storage) {
+    const rate = storage <= 10
+      ? 4
+      : storage <= 100
+        ? 2
+        : 1;
+    return rate * storage * 100;
+  }
